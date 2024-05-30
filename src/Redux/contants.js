ca4a2bf681dd023ab45cb95367b7a65573552cef
ravisurani0 <115344382+ravisurani0@users.noreply.github.com>
2024-05-30 23:31:53 +0530

    Update contants.js

--- src/Redux/contants.js
+++ src/Redux/contants.js
@@ -12,5 +12,5 @@ export const ActionTypes = {
 };
 // BASE URLS
 export const SERVICES_BASE_URL = "https://docs.google.com/spreadsheets/d/e/2PACX-1vQoHw-Ea9lbL4wpLOU4WjhWY0dXCeIjjBIBn71Y9ypTzIRKLuwZjk29Ef0LHCuPOqgVoSVSB2z_MyuO/pub?output=csv";
-export const BLOGS_BASE_URL = "https://docs.google.com/spreadsheets/d/e/2PACX-1vTOM2TIoVjpn2uxYndkBSxIF1051NkZa9CABn4eNRA8dQanjs1603WLoq7-W0IsGyFmG8KiWS9Vg1RP/pub?output=csv";
+export const BLOGS_BASE_URL = "https://docs.google.com/spreadsheets/d/e/2PACX-1vQoHw-Ea9lbL4wpLOU4WjhWY0dXCeIjjBIBn71Y9ypTzIRKLuwZjk29Ef0LHCuPOqgVoSVSB2z_MyuO/pub?output=csv";
 
